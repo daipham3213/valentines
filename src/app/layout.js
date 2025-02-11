4add3f94289fc2d3851f9cbaff8451ce1a8e50db
Pham Le Gia Dai <daiplg@fpt.com>
2025-02-11 23:48:18 +0700

    Init

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+import "./globals.css";
+
+
+export const metadata = {
+  title: "Valentines 2024",
+  description: "Đại sent you a letter!",
+  metadataBase: new URL('https://cuaa.fn.run.place'),
+  openGraph: {
+    images: "heart.png",
+  }
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
